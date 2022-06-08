181919f8ee54a4e4db525487afa4f8c22007cd1e
JohanJimenex <johan.jimenez.23@gmail.com>
2022-06-07 22:04:40 -0400

    base var added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public titulo: string = 'Contador App';
-
   public numero: number = 0;
+  public base: number = 5
 
   modificarContador(valor: number): void {
     this.numero += valor;
