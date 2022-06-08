41108f1c2af3684438f710927959a3bac4bb63b6
JohanJimenex <johan.jimenez.23@gmail.com>
2022-06-07 21:38:04 -0400

    Mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //nombre del componente , se usa en el html principal
+  // template: '<h1>heelllo</h1>', //Agrega codigo html 
+  templateUrl: './app.component.html', //ruta del archivo que se suara como componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+  public titulo: string = 'Contador App';
+
+  public numero: number = 0;
+
+  modificarContador(valor: number): void {
+    this.numero += valor;
+  }
+
+
+
 }
